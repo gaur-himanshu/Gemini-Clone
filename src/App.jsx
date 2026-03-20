c9e37946f5e90969efd3c7b8910f8b9971087b82
gaur-himanshu <gaur.himanshu0407@gmail.com>
2026-03-20 21:41:25 +0530

    Sidebar UI implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
 export default function App() {
-  return <div>
-    
-  </div>;
+  return <>
+  <Sidebar/>
+  </>;
 }
