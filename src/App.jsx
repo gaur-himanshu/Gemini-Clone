053a5d59b22955f2e365fc8dbd2a4219127f7e8b
gaur-himanshu <gaur.himanshu0407@gmail.com>
2026-03-21 17:54:31 +0530

    Main component UI implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
 export default function App() {
   return <>
   <Sidebar/>
+  <Main/>
   </>;
 }
